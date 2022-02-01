e34e9bfbd8f3da486a94d11454e390d656569c35
juliashovhalova2010 <83767399+juliashovhalova2010@users.noreply.github.com>
2022-02-01 10:27:09 +0300

    Update TransactionsWidget.js

--- public/js/ui/widgets/TransactionsWidget.js
+++ public/js/ui/widgets/TransactionsWidget.js
@@ -11,16 +11,31 @@ class TransactionsWidget {
    * Если переданный элемент не существует,
    * необходимо выкинуть ошибку.
    * */
-  constructor( element ) {
 
+ constructor(element) {
+    if (!element) {
+      throw new Error('Элемент не существует');
+    }
+    this.element = element;
+    this.registerEvents();
   }
-  /**
-   * Регистрирует обработчики нажатия на
-   * кнопки «Новый доход» и «Новый расход».
-   * При нажатии вызывает Modal.open() для
-   * экземпляра окна
-   * */
-  registerEvents() {
+  // /**
+  //  * Регистрирует обработчики нажатия на
+  //  * кнопки «Новый доход» и «Новый расход».
+  //  * При нажатии вызывает Modal.open() для
+  //  * экземпляра окна
+  //  * */
+ registerEvents() {
+    const createIncomeButton = this.element.querySelector('.create-income-button'),
+      createExpenseButton = this.element.querySelector('.create-expense-button'),
+
+      incomeModal = App.getModal('newIncome'),
+      expenseModal = App.getModal('newExpense');
+
+    createIncomeButton.addEventListener('click', () => incomeModal.open());
+    createExpenseButton.addEventListener('click', () => expenseModal.open());
+
+
 
   }
 }

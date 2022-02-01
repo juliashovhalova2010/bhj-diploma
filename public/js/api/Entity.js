2a39049792e59c576542ef7f4c8ea24d65909c56
juliashovhalova2010 <83767399+juliashovhalova2010@users.noreply.github.com>
2022-02-01 10:17:44 +0300

    Update Entity.js

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -2,30 +2,40 @@
  * Класс Entity - базовый для взаимодействия с сервером.
  * Имеет свойство URL, равно пустой строке.
  * */
+
+
 class Entity {
   /**
    * Запрашивает с сервера список данных.
    * Это могут быть счета или доходы/расходы
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static list(data, callback){
+
+  static URL = '';
+  static list(data, callback = f => f) {
+    let options = { data: data, url: `${this.URL}`, method: `GET`, responseType: 'json', callback: callback };
+    return createRequest(options);
 
   }
-
   /**
    * Создаёт счёт или доход/расход с помощью запроса
    * на сервер. (в зависимости от того,
    * что наследуется от Entity)
    * */
-  static create(data, callback) {
 
+  static create(data, callback = f => f) {
+    let options = { data: data, url: `${this.URL}`, method: `PUT`, responseType: 'json', callback: callback };
+    return createRequest(options);
   }
 
   /**
    * Удаляет информацию о счёте или доходе/расходе
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static remove(data, callback ) {
-
+  static remove(data, callback = f => f) {
+    let options = { data: data, url: `${this.URL}`, method: `DELETE`, responseType: 'json', callback: callback };
+    console.log(options)
+    return createRequest(options);
   }
+
 }

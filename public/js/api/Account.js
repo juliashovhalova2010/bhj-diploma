d728dce3a57f56a2e3269c7278ad8f5e4096904c
juliashovhalova2010 <83767399+juliashovhalova2010@users.noreply.github.com>
2022-02-01 10:17:15 +0300

    Update Account.js

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -3,11 +3,23 @@
  * Управляет счетами пользователя.
  * Имеет свойство URL со значением '/account'
  * */
+
 class Account extends Entity {
+
+  static URL = '/account';
   /**
    * Получает информацию о счёте
    * */
-  static get(id = '', callback){
 
+  static get(id = '', callback = f => f) {
+    let options = {
+      url: `${this.URL}/${id}`,
+      responseType: 'json',
+      data: null, 
+      method: 'GET',
+      callback: callback
+    }
+    createRequest(options)
+    console.log(options)
   }
 }
